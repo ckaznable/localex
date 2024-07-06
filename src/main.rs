8fb53ffdaffb18e063cfbeebd2b5fed899114f81
ckaznable <ckaznable@gmail.com>
2024-07-06 16:46:42 +0800

    refactor: tidy main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
-use behaviour::LocalExBehaviour;
+use behaviour::{LocalExBehaviour, LocalExBehaviourEvent};
 use futures::StreamExt;
 use libp2p::swarm::SwarmEvent;
-use libp2p::{identity, SwarmBuilder};
+use libp2p::{identity, mdns, SwarmBuilder};
 use secret::SecretStore;
 
 mod behaviour;
@@ -11,12 +11,18 @@ mod secret;
 #[tokio::main]
 async fn main() -> Result<()> {
     let store = SecretStore::new().await?;
+
     let local_key = store.get_local_key().await.unwrap_or_else(|| {
         println!("local key not existing, generated new one");
         identity::Keypair::generate_ed25519()
     });
 
+    let password: Vec<u8> = store.get_password().await.unwrap_or_else(|| {
+        rpassword::prompt_password("Please enter verification password: ").unwrap().as_bytes().to_vec()
+    });
+
     store.save_local_key(&local_key).await?;
+    store.save_password(&password).await?;
 
     let mut swarm = SwarmBuilder::with_existing_identity(local_key)
         .with_tokio()
@@ -32,8 +38,22 @@ async fn main() -> Result<()> {
 
     loop {
         match swarm.select_next_some().await {
-            SwarmEvent::NewListenAddr { address, .. } => println!("Listening on {address:?}"),
-            SwarmEvent::Behaviour(event) => println!("{event:?}"),
+            SwarmEvent::NewListenAddr { address, .. } => println!("Local node is listening on {address}"),
+            SwarmEvent::Behaviour(event) => match event {
+                LocalExBehaviourEvent::Gossipsub(_) => todo!(),
+                LocalExBehaviourEvent::Mdns(mdns::Event::Discovered(list)) => {
+                    for (peer_id, _) in list {
+                        println!("mDNS discovered a new peer: {peer_id}");
+                        swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
+                    }
+                },
+                LocalExBehaviourEvent::Mdns(mdns::Event::Expired(list)) => {
+                    for (peer_id, _) in list {
+                        println!("mDNS discover peer has expired: {peer_id}");
+                        swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer_id);
+                    }
+                },
+            },
             _ => {}
         }
     }
