f84270ebf02e27a5aceea4fadabd1ee1cdaf7d65
ckaznable <ckaznable@gmail.com>
2024-07-06 16:45:30 +0800

    feat: impl secret store

--- src/secret.rs
+++ src/secret.rs
@@ -5,23 +5,25 @@ use libp2p::identity::Keypair;
 use secret_service::{EncryptionType, SecretService};
 
 const SECRET_LABEL: &str = "LocalEx";
+const ATTR_KEY: &str = "localex";
+const PRIVATE_KEY_ATTR: (&str, &str) = (ATTR_KEY, "keypair");
+const PASSWORD_ATTR: (&str, &str) = (ATTR_KEY, "password");
 
 pub struct SecretStore<'a> {
     service: SecretService<'a>,
-    attributes: HashMap<&'a str, &'a str>,
 }
 
 impl<'a> SecretStore<'a> {
     pub async fn new() -> Result<Self> {
         Ok(Self {
             service: SecretService::connect(EncryptionType::Dh).await?,
-            attributes: HashMap::from([("localex", "key")]),
         })
     }
 
     pub async fn get_local_key(&self) -> Option<Keypair> {
-        let search_items = self.service
-            .search_items(self.attributes.clone())
+        let search_items = self
+            .service
+            .search_items(HashMap::from([PRIVATE_KEY_ATTR]))
             .await
             .ok()?;
 
@@ -34,13 +36,41 @@ impl<'a> SecretStore<'a> {
         let key = keypair.to_protobuf_encoding()?;
         let collection = self.service.get_default_collection().await?;
 
-        collection.create_item(
-            SECRET_LABEL,
-            self.attributes.clone(),
-            key.as_slice(),
-            true,
-            "text/plain"
-        ).await?;
+        collection
+            .create_item(
+                SECRET_LABEL,
+                HashMap::from([PRIVATE_KEY_ATTR]),
+                key.as_slice(),
+                true,
+                "text/plain",
+            )
+            .await?;
+
+        Ok(())
+    }
+
+    pub async fn get_password(&self) -> Option<Vec<u8>> {
+        let search_items = self
+            .service
+            .search_items(HashMap::from([PASSWORD_ATTR]))
+            .await
+            .ok()?;
+
+        let item = search_items.unlocked.first()?;
+        item.get_secret().await.ok()
+    }
+
+    pub async fn save_password(&self, password: &[u8]) -> Result<()> {
+        let collection = self.service.get_default_collection().await?;
+        collection
+            .create_item(
+                SECRET_LABEL,
+                HashMap::from([PASSWORD_ATTR]),
+                password,
+                true,
+                "text/plain",
+            )
+            .await?;
 
         Ok(())
     }
