ea7aa2b9f9c32f7bf54d074e817ccc1cf94d1748
ckaznable <ckaznable@gmail.com>
2024-07-16 17:31:55 +0800

    chore: add logger package

---
+++ packages/logger/src/lib.rs
@@ -0,0 +1,50 @@
+use anyhow::Result;
+use tracing::info;
+use tracing_subscriber::{
+    fmt::{self, format::FmtSpan},
+    EnvFilter,
+    prelude::*,
+};
+use tracing_appender::rolling::{RollingFileAppender, Rotation};
+
+#[derive(Default)]
+pub struct LoggerConfig<'a> {
+    pub filename: &'a str,
+    pub stdout: bool,
+}
+
+pub fn init_logger(config: LoggerConfig) -> Result<()> {
+    let mut log_dir = dirs::cache_dir().unwrap();
+    log_dir.push("localex");
+    let file_appender = RollingFileAppender::new(
+        Rotation::NEVER,
+        log_dir,
+        config.filename,
+    );
+
+    let file_layer = fmt::layer()
+        .with_writer(file_appender)
+        .with_ansi(true)
+        .with_span_events(FmtSpan::CLOSE)
+        .event_format(fmt::format().compact());
+
+    let subscriber = tracing_subscriber::registry()
+        .with(EnvFilter::try_from_default_env()
+            .unwrap_or_else(|_| EnvFilter::new("info")))
+        .with(file_layer);
+
+    if config.stdout {
+        let stdout_layer = fmt::layer()
+            .with_writer(std::io::stdout)
+            .with_ansi(true)
+            .with_span_events(FmtSpan::CLOSE)
+            .event_format(fmt::format().compact());
+
+        subscriber.with(stdout_layer).init();
+    } else {
+        subscriber.init();
+    }
+
+    info!("Logger initialized successfully");
+    Ok(())
+}
