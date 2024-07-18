bf08773e10cf826f15e7fab9fbe748b6a01245f7
ckaznable <ckaznable@gmail.com>
2024-07-18 14:19:24 +0800

    fix: tui main

--- packages/localex-tui/src/main.rs
+++ packages/localex-tui/src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
+use clap::Parser;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    localex_tui::main().await
+    let param = localex_tui::cli::Cli::parse();
+    localex_tui::main(param.into()).await
 }
