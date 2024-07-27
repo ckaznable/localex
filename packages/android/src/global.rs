d7f3e7b3dac7601d77ad2e417ac48b51618df7a9
ckaznable <ckaznable@gmail.com>
2024-07-27 16:03:27 +0800

    fix: get service error

--- packages/android/src/global.rs
+++ packages/android/src/global.rs
@@ -1,11 +1,10 @@
 use std::sync::Arc;
 
-use anyhow::{Result, anyhow};
 use libp2p::identity::Keypair;
 use protocol::event::DaemonEvent;
 use tokio::sync::{mpsc, Mutex};
 
-use crate::service::ServiceManager;
+use crate::{error::FFIError, service::ServiceManager};
 
 type ChannelSender<T> = mpsc::Sender<T>;
 type ChannelReceiver<T> = Arc<Mutex<mpsc::Receiver<T>>>;
@@ -15,32 +14,32 @@ pub static mut SERVICE: Option<Arc<Mutex<ServiceManager>>> = None;
 pub static mut SENDER: Option<ChannelSender<DaemonEvent>> = None;
 pub static mut RECEIVER: Option<ChannelReceiver<DaemonEvent>> = None;
 
-pub fn get_service() -> Result<Arc<Mutex<ServiceManager>>> {
+pub fn get_service() -> Result<Arc<Mutex<ServiceManager>>, FFIError> {
     get_or_create_service(None, None, None)
 }
 
-pub fn get_or_create_service(keypair: Option<Keypair>, hostname: Option<String>, daemon_tx: Option<mpsc::Sender<DaemonEvent>>) -> Result<Arc<Mutex<ServiceManager>>> {
+pub fn get_or_create_service(keypair: Option<Keypair>, hostname: Option<String>, daemon_tx: Option<mpsc::Sender<DaemonEvent>>) -> Result<Arc<Mutex<ServiceManager>>, FFIError> {
     unsafe {
         if keypair.is_none() && SERVICE.is_none() && daemon_tx.is_none() {
-            return Err(anyhow!("can't get service before initializing"))
+            return Err(FFIError::AccessServiceBeforeInitError)
         }
 
-        let keypair = keypair.ok_or_else(|| anyhow!("keypair is none"))?;
         SERVICE.clone().map(Ok).unwrap_or_else(|| {
-            let service = Arc::new(
-                Mutex::new(
-                    ServiceManager::new(
-                        keypair,
-                        hostname.unwrap_or_else(|| String::from("unknown")),
-                        daemon_tx.unwrap(),
-                    )?));
+            let keypair = keypair.ok_or(FFIError::Unknown)?;
+            let service = ServiceManager::new(
+                keypair,
+                hostname.unwrap_or_else(|| String::from("unknown")),
+                daemon_tx.unwrap(),
+            ).map_err(|_| FFIError::CreateServiceError)?;
+
+            let service = Arc::new(Mutex::new(service));
             SERVICE = Some(service.clone());
             Ok(service)
         })
     }
 }
 
-pub fn get_or_create_channel() -> Result<(ChannelSender<DaemonEvent>, ChannelReceiver<DaemonEvent>)> {
+pub fn get_or_create_channel() -> Result<(ChannelSender<DaemonEvent>, ChannelReceiver<DaemonEvent>), FFIError> {
     unsafe {
         SENDER
             .clone()
