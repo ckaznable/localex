a2a2e710e80f95733e655534dfa328b8b9249b71
ckaznable <ckaznable@gmail.com>
2024-08-24 16:10:23 +0800

    feat: added helper method for db

--- database/src/lib.rs
+++ database/src/lib.rs
@@ -1,11 +1,19 @@
-use std::path::PathBuf;
-
+use anyhow::anyhow;
 use anyhow::Result;
+use sea_orm::Condition;
+use sea_orm::QueryFilter;
+use sea_orm::entity::{ColumnTrait, ModelTrait};
+use sea_orm::Set;
+use std::path::PathBuf;
+use entity::regist_file;
+use entity::regist_file::Entity as RegistFile;
+use entity::regist_file::Model as RegistFileModel;
+
 use migration::{Migrator, MigratorTrait};
-use sea_orm::{Database, DatabaseConnection};
+use sea_orm::{Database, DatabaseConnection, EntityTrait};
 
 pub struct LocalExDb {
-    pub db: DatabaseConnection,
+    db: DatabaseConnection,
 }
 
 impl LocalExDb {
@@ -23,5 +31,81 @@ impl LocalExDb {
         Migrator::up(&db, None).await?;
 
         Ok(Self {db})
+    }
+
+    pub async fn get_all_regist_files(&self) -> Result<Vec<RegistFileModel>> {
+        RegistFile::find().all(&self.db).await.map_err(anyhow::Error::from)
+    }
+
+    pub async fn get_all_regist_files_by_app(&self, app_id: &str) -> Result<Vec<RegistFileModel>> {
+        RegistFile::find()
+            .filter(regist_file::Column::AppId.eq(app_id))
+            .all(&self.db)
+            .await.map_err(anyhow::Error::from)
+    }
+
+    pub async fn get_file_model(&self, app_id: &str, file_id: &str) -> Option<RegistFileModel> {
+        RegistFile::find()
+            .filter(
+                Condition::all()
+                    .add(regist_file::Column::FileId.eq(file_id))
+                    .add(regist_file::Column::AppId.eq(app_id))
+            )
+            .one(&self.db)
+            .await
+            .ok()
+            .flatten()
+    }
+
+    pub async fn regist_file(&self, app_id: &str, file_id: &str, path: &str) -> Result<()> {
+        if self.get_file_model(app_id, file_id).await.is_some() {
+            return Err(anyhow!("already exists: {} {}", app_id, file_id));
+        }
+
+        let pear = regist_file::ActiveModel {
+            app_id: Set(app_id.to_owned()),
+            file_id: Set(file_id.to_owned()),
+            path: Set(path.to_owned()),
+            ..Default::default()
+        };
+
+        RegistFile::insert(pear).exec(&self.db).await?;
+        Ok(())
+    }
+
+    pub async fn unregist_file(&self, app_id: &str, file_id: &str) -> Result<()> {
+        if let Some(f) = self.get_file_model(app_id, file_id).await {
+            f.delete(&self.db).await?;
+        }
+
+        Ok(())
+    }
+
+    pub async fn update_version(&self, app_id: &str, file_id: &str, version: i32) -> Result<()> {
+        let pear = regist_file::ActiveModel {
+            app_id: Set(app_id.to_owned()),
+            file_id: Set(file_id.to_owned()),
+            version: Set(version),
+            ..Default::default()
+        };
+
+        RegistFile::update(pear).exec(&self.db).await?;
+        Ok(())
+    }
+
+    pub async fn update_path(&self, app_id: &str, file_id: &str, path: &str) -> Result<()> {
+        let pear = regist_file::ActiveModel {
+            app_id: Set(app_id.to_owned()),
+            file_id: Set(file_id.to_owned()),
+            path: Set(path.to_owned()),
+            ..Default::default()
+        };
+
+        RegistFile::update(pear).exec(&self.db).await?;
+        Ok(())
+    }
+
+    pub async fn get_file_path(&self, app_id: &str, file_id: &str) -> Option<String> {
+        self.get_file_model(app_id, file_id).await.map(|m| m.path)
     }
 }
